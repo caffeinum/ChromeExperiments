2654361a6e98198545bb22d59c324137e3ce0d90
Aleksey Bykhun <theisease@gmail.com>
2019-07-24 09:59:10 +0300

    update

---
+++ 05-js/script.js
@@ -0,0 +1,9 @@
+// const right = JSON.parse('{"foo": "bar"}')
+// const wrong = JSON.parse('{foo: "bar"}')
+
+// try {
+//     const right = JSON.parse('{"foo": "bar"}')
+//     const wrong = JSON.parse('{foo: "bar"}')
+// } catch (err) {
+//     console.log(err)
+// }
